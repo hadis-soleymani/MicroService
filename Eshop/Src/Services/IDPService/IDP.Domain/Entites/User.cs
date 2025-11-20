0e06a4fc49ddba0edcb312af8162509a7634e994
hadis-soleymani <hadiissoleymani@gmail.com>
2025-11-20 09:04:48 +0330

    add interfaces

--- Eshop/Src/Services/IDPService/IDP.Domain/Entites/User.cs
+++ Eshop/Src/Services/IDPService/IDP.Domain/Entites/User.cs
@@ -5,5 +5,11 @@
         public required string FullName { get; set; }
 
         public required string CodeNumber { get; set; }
+
+        public required string UserName { get; set; }
+
+        public required string Password { get; set; }
+
+        public required string Salt { get; set; }
     }
 }
---
+++ Eshop/Src/Services/IDPService/IDP.Domain/IRepository/Command/IUserCommandRepository.cs
@@ -0,0 +1,9 @@
+﻿using IDP.Domain.Entites;
+using IDP.Domain.IRepository.Command.Base;
+
+namespace IDP.Domain.IRepository.Command
+{
+    public interface IUserCommandRepository: ICommandRepository<User>
+    {
+    }
+}
