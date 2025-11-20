39df58ba0797abf87f886b14607ec3a33d9e8ce0
hadis-soleymani <hadiissoleymani@gmail.com>
2025-11-20 08:38:02 +0330

    add api version

--- Eshop/Src/Services/IDPService/IDP.Api/Program.cs
+++ Eshop/Src/Services/IDPService/IDP.Api/Program.cs
@@ -1,6 +1,5 @@
+using Asp.Versioning;
 using IDP.Application.Handler.Command.User;
-using IDP.Domain.IRepository.Command;
-using IDP.Infra.Repository.Command;
 using MediatR;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,7 +9,21 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddMediatR(typeof(UserHandler).Assembly);
-builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddApiVersioning(options =>
+{
+    options.DefaultApiVersion = new ApiVersion(1);
+    options.ReportApiVersions = true;
+    options.AssumeDefaultVersionWhenUnspecified = true;
+    options.ApiVersionReader = ApiVersionReader.Combine(
+        new UrlSegmentApiVersionReader(),
+        new HeaderApiVersionReader("X-Api-Version"));
+})
+.AddMvc() // This is needed for controllers
+.AddApiExplorer(options =>
+{
+    options.GroupNameFormat = "'v'V";
+    options.SubstituteApiVersionInUrl = true;
+});
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
--- Eshop/Src/Services/IDPService/IDP.Application/Handler/Command/User/UserHandler.cs
+++ Eshop/Src/Services/IDPService/IDP.Application/Handler/Command/User/UserHandler.cs
@@ -1,18 +1,10 @@
 ﻿using IDP.Application.Command.User;
-using IDP.Domain.IRepository.Command;
 using MediatR;
 
 namespace IDP.Application.Handler.Command.User
 {
     public class UserHandler : IRequestHandler<UserCommand, bool>
     {
-        private readonly IUserRepository _userRepository;
-
-        public UserHandler(IUserRepository userRepository)
-        {
-            _userRepository = userRepository;
-        }
-
         public async Task<bool> Handle(UserCommand request, CancellationToken cancellationToken)
         {
             return true;
